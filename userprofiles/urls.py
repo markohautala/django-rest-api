911d8ea052bee9173813f96d49b65d41f90c1eb4
markohautala <markohautala@live.se>
2024-08-01 23:41:55 +0200

    Add PUT HTTP functionality to our views.py and backend

--- userprofiles/urls.py
+++ userprofiles/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import UserProfileView  # Import UserProfileView
+from .views import UserProfileView, UserProfileDetailView  # Import UserProfileView and UserProfileDetailView
+
 
 urlpatterns = [
     path('', UserProfileView.as_view(), name='home'),  # Add a URL pattern for the root path
     path('userprofile/', UserProfileView.as_view(), name='userprofile'),  # URL for user profiles
+    path('userprofile/<int:pk>/', UserProfileDetailView.as_view(), name='userprofile-detail'),  # URL for user profile details
 ]
--- userprofiles/views.py
+++ userprofiles/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import UserProfile  # Import the model here
@@ -6,6 +8,26 @@ from .serializers import UserProfileSerializer  # Import the serializer here
 class UserProfileView(APIView):  # Renamed to avoid confusion with the model name
     def get(self, request):
         user_profiles = UserProfile.objects.all()  # Use the model's name to query
-        # Serializing the queryset is necessary to return JSON responses
         serializer = UserProfileSerializer(user_profiles, many=True)
         return Response(serializer.data)
+
+class UserProfileDetailView(APIView):  # Renamed to avoid confusion with the model name
+    serializer_class = UserProfileSerializer
+    def get_object(self, pk):
+        try:
+            return UserProfile.objects.get(pk=pk)  # primary key (pk) is used to query a single instance
+        except UserProfile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        user_profile = self.get_object(pk)
+        serializer = UserProfileSerializer(user_profile)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        user_profile = self.get_object(pk)
+        serializer = UserProfileSerializer(user_profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
