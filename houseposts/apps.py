d829432b47b94a7b6407a741919df678fa6ffbc0
markohautala <markohautala@live.se>
2024-08-02 13:59:02 +0200

    Add app houseposts and add into installed apps. Fill out models.py

---
+++ houseposts/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HousepostsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'houseposts'
---
+++ houseposts/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class HousePost(models.Model):
+    """
+    HousePost model, associated with 'poster', i.e., a User instance.
+    Default image set so that we can always reference image.url.
+    """
+    poster = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    house_title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    house_image = models.ImageField(
+        upload_to='house_images/', default='../house-placeholder-image_vgm8en', blank=True
+    )
+
+    class Meta:
+        ordering = ['-date_posted']
+
+    def __str__(self):
+        return f'{self.id} {self.house_title}'
