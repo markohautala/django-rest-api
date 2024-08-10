d9f2aa15ed776795807692ad1f434622c7bd06b1
markohautala <markohautala@live.se>
2024-08-10 14:39:01 +0200

    Update static file configuration for Django project

--- restapi/settings.py
+++ restapi/settings.py
@@ -172,7 +172,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = 'static/'
+STATIC_ROOT = '/home/django/django_project/django_project/static'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
