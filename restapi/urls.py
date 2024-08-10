a17bae53437edf39e980ecf12bc7d147e3d6d109
markohautala <markohautala@live.se>
2024-08-10 15:51:07 +0200

    Update .gitignore to ignore additional files and directories

--- restapi/urls.py
+++ restapi/urls.py
@@ -6,11 +6,14 @@ urlpatterns = [
     path('', root_route),  # Include the root_route view at the root URL
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),  # Include the URL patterns from the rest_framework app
-    path('', include('userprofiles.urls')),  # Include the URL patterns from the userprofiles app
-    path('', include('houseposts.urls')),  # Include the URL patterns from the houseposts app
-    path('', include('housepostcomments.urls')),  # Include the URL patterns from the housepostcomments app
-    path('', include('househearts.urls')),  # Include the URL patterns from the househearts app
-    path('dj-rest-auth/', include('dj_rest_auth.urls')), # Include the URL patterns from the dj_rest_auth app
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')), # Include the URL patterns from the dj_rest_auth.registration app
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),  # Include the URL patterns from the dj_rest_auth app
+
+    # Include the URL patterns from each app with unique prefixes
+    path('userprofiles/', include('userprofiles.urls')),
+    path('houseposts/', include('houseposts.urls')),
+    path('housepostcomments/', include('housepostcomments.urls')),
+    path('househearts/', include('househearts.urls')),
+
+    # Include the URL patterns from the dj_rest_auth app
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
