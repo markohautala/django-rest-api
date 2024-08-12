64f782ac7ec63d9c6416a1cc331ee3887bdedb93
markohautala <markohautala@live.se>
2024-08-13 00:16:48 +0200

    Update DEBUG setting to use DEV environment variable

--- restapi/settings.py
+++ restapi/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')  # Use a default only for development
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEV', '0') == '1'
 
 ALLOWED_HOSTS = ['*']  # Allow any host
 
