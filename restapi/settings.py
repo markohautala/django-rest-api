f9b99210cf37ea1790cdfaab7a344ca2b9b32a2a
markohautala <markohautala@live.se>
2024-08-13 12:17:02 +0200

    Update JWT_AUTH_SECURE setting to False

--- restapi/settings.py
+++ restapi/settings.py
@@ -115,7 +115,7 @@ if 'DEV' not in os.environ:
 
 
 REST_USE_JWT = True
-JWT_AUTH_SECURE = not DEBUG # Set to True in production
+JWT_AUTH_SECURE = False
 JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'  # Required for cross-origin cookie sharing
