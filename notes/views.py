5856f42be495ca1fad08dbc37b7adb30a0552e4f
markohautala <markohautala@live.se>
2024-10-06 17:40:55 +0200

    Refactor NoteList view to handle unauthenticated access for creating notes

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,4 @@
-# notes/views.py
-
-from django.db.models import Count
+from django.shortcuts import redirect
 from rest_framework import generics, permissions
 from .models import Note
 from .serializers import NoteSerializer
@@ -10,16 +8,26 @@ class NoteList(generics.ListCreateAPIView):
     """
     List all notes for the authenticated user, or create a new note.
     """
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]  # Allow unauthenticated access for creating notes
     serializer_class = NoteSerializer
 
     def get_queryset(self):
-        # Filter notes to return only those belonging to the logged-in user
-        return Note.objects.filter(user=self.request.user).order_by('-date_created')
+        # Ensure that only the notes belonging to the authenticated user are accessible
+        return Note.objects.filter(user=self.request.user)
+
+    def get(self, request, *args, **kwargs):
+        # Check if the user is authenticated
+        if not request.user.is_authenticated:
+            # Redirect to the login page if the user is not authenticated
+            return redirect("/api-auth/login/?next=/notes/")
+
+        # Proceed with the standard get behavior if the user is authenticated
+        return super().get(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         # Save the logged-in user as the note owner
         serializer.save(user=self.request.user)
+
 
 class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
     """
